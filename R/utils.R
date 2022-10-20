b8283e5b1c2ebb0cf041d016b58b9a44ba209a7e
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2022-10-21 00:38:31 +0900

    check_packages function is updated

--- R/utils.R
+++ R/utils.R
@@ -262,6 +262,9 @@ check_packages  <-  function( pkgs = NULL ){
 
     if ( is.null( pkgs ) ) {
         pkgs  =  list(  actuar = 'rztpois',
+                        dplyr  = '%>%',
+                        ggplot2  =  c( 'aes', 'geom_boxplot', 'geom_point', 'geom_violin',
+                                       'ggplot', 'ggtitle', 'theme', 'xlab', 'ylab' ),
                         graphics = c('axis', 'legend', 'lines', 'par', 'plot', 'text', 'title' ),
                         grDevices = c('dev.off', 'pdf', 'rgb'),
                         methods = 'new',
